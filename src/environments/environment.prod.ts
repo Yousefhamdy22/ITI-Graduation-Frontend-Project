46296d91d366200f201c7949e70b682cc887aa57
Yousefhamdy22 <138791316+Yousefhamdy22@users.noreply.github.com>
2025-11-23 14:06:35 +0200

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.elearning.com',
-  apiTimeout: 30000,
-  tokenKey: 'elearning_access_token',
-  refreshTokenKey: 'elearning_refresh_token',
-  defaultLanguage: 'en',
-  supportedLanguages: ['en', 'ar']
-};
